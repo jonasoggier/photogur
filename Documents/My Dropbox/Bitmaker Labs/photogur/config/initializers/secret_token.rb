4ac29be1fd7313c996e1df2ee2237abde75ef8e2
jonasoggier <jonas.oggier@gmail.com>
2013-03-19 18:33:39 -0400

    Blank slate

---
+++ Documents/My Dropbox/Bitmaker Labs/photogur/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Photogur::Application.config.secret_token = '3b94cb877ed147a1f294ede5f2971588ee7a9793b83598998ab2e7cf76df0f03da1c5b872a7bafe4cb98a6a5ca791dc9e2727c476d2e5bae5a2a63cdc16a1953'
